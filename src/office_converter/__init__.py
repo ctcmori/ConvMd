d3b4e5885bd7eaae1d52e7e9e2cd077dee03301e
ctcmori <masahiko.mori@ctc-g.co.jp>
2025-09-26 21:56:03 +0900

    🔧 基本設定ファイルとプロンプト追加: 設定テンプレートとDify変換プロンプト

---
+++ src/office_converter/__init__.py
@@ -0,0 +1,8 @@
+"""Office Converter Package
+
+Office文書をPDF経由でMarkdownに変換するパッケージ
+"""
+
+__version__ = "1.0.0"
+__author__ = "CTC Skill Task Force"
+__description__ = "Office Document to Markdown Converter via PDF"
